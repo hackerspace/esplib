c1cf2962af3b30d8ffc80e73dd78aecd6012b838
Richard Marko <rmarko@fedoraproject.org>
2016-04-19 06:15:59 +0200

    update telnetsrv with buffering to work with devel nodemcu

--- luatool/telnetsrv.lua
+++ luatool/telnetsrv.lua
@@ -1,38 +1,29 @@
---
--- setup a telnet server that hooks the sockets input
---
-function telnetsrv()
-    in_use = false
-    function listenFun(sock)
-        if in_use then
-            sock:send("Already in use.\n")
-            sock:close()
-            return
+telnetsrv = net.createServer(net.TCP, 180)
+telnetsrv:listen(23, function(socket)
+    local fifo = {}
+    local fifo_drained = true
+    local function sender(c)
+        if #fifo > 0 then
+            c:send(table.remove(fifo, 1))
+        else
+            fifo_drained = true
         end
-        in_use = true
-
-        function s_output(str)
-            if(sock ~=nil) then
-                sock:send(str)
-            end
-        end
-
-        node.output(s_output, 0)
-
-        sock:on("receive", function(sock, input)
-                node.input(input)
-            end)
-
-        sock:on("disconnection", function(sock)
-                node.output(nil)
-                in_use = false
-            end)
     end
+    local function s_output(str)
+        table.insert(fifo, str)
+        if socket ~= nil and fifo_drained then
+            fifo_drained = false
+            sender(socket)
+        end
+    end
+    node.output(s_output, 0)   -- re-direct output to function s_ouput.
+    socket:on("receive", function(c, l)
+        node.input(l)           -- works like pcall(loadstring(l)) but support multiple separate line
+    end)
+    socket:on("disconnection", function(c)
+        node.output(nil)        -- un-regist the redirect output function, output goes to serial
+    end)
+    socket:on("sent", sender)
+end)
 
-    telnetServer = net.createServer(net.TCP, 180)
-    telnetServer:listen(23, listenFun)
-
-    print('Telnet server started')
-end
-
-telnetsrv()
+--telnetsrv()
