1a7fb8d30c3b638ebffc8fd3cfb9c876f2b06865
Richard Marko <rmarko@fedoraproject.org>
2016-07-31 21:26:02 +0200

    ws2812 wifi

---
+++ luatool/main_ws.lua
@@ -0,0 +1,16 @@
+print('main.lua')
+
+tsrv = nil
+
+d('telnetsrv')
+
+-- d('blink')
+-- out(out1)
+-- out(out2)
+-- out(debug_led)
+-- blink(debug_led)
+
+ws2812.init()
+d('wifi_ws2812')
+
+print('/main.lua')
--- luatool/wifi_ws2812.lua
+++ luatool/wifi_ws2812.lua
@@ -1,8 +1,31 @@
-tmr.alarm(0, 500, 1,function() ws2812.write(0, string.char(0, (wifi.sta.getrssi() + 100)*6, 0):rep(16)) end)
+--tmr.alarm(0, 100, 1,function()
+--  c = string.char(100, 100, 0):rep(50)
+--  ws2812.write(c)
+--  i = wifi.sta.getrssi()
+--  c = string.char(0, 200, 0):rep(i/-2)
+--  ws2812.write(c) end)
 
-local i = 0; b = ws2812.newBuffer(16, 3); b:fill(255, 255, 0); tmr.alarm(0, 50, 1, function()
-        i=i+1
-        b:fade(2)
-        b:set(i%b:size(), 255, 0, 0)
-        b:write(0)
+function scale(x, in_min, in_max, out_min, out_max)
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+end
+
+rssimin = 100
+rssimax = 0
+leds = 16
+
+local b = ws2812.newBuffer(leds + 10, 3); b:fill(0, 0, 0); tmr.alarm(0, 1000, 1, function()
+  rssi = wifi.sta.getrssi() * -1
+  if rssi > rssimax then rssimax = rssi end
+  if rssi < rssimin then rssimin = rssi end
+
+  rssi = scale(rssi, rssimin, rssimax, 0, leds)
+
+  for i=1,leds do
+    if rssi < i then
+      b:set(i, 255, 0, 0)
+    else
+      b:set(i, 0, 255, 0)
+    end
+    b:write(0)
+  end
 end)
