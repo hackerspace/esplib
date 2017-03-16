50b9198247e58d55f88ad9ff04d7dbcae8bfc2c9
Richard Marko <rmarko@fedoraproject.org>
2017-03-16 22:46:51 +0100

    set hostname with wifi.sta.sethostname(hostname)

--- luatool/wifi_client.lua
+++ luatool/wifi_client.lua
@@ -2,5 +2,9 @@ print('set mode=STATION (mode='..wifi.getmode()..')')
 print('MAC: ', wifi.sta.getmac())
 print('chip: ', node.chipid())
 print('heap: ', node.heap())
+print('hostname: ', hostname)
 
+wifi.sta.sethostname(hostname)
 wifi.sta.config(wifi_ssid, wifi_pass)
+-- FIXME: check if supported
+-- wifi.sta.sleeptype(wifi_sleep_type)
