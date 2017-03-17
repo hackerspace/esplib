d7054bbf95a85444151d368c534dbdb1bf36ae8b
Richard Marko <rmarko@fedoraproject.org>
2017-03-17 02:13:25 +0100

    add hostname and wifi_sleep_type config vars, disable stationap

--- luatool/config.lua
+++ luatool/config.lua
@@ -1,11 +1,15 @@
+hostname = "esp"
 -- node_cpu = node.CPU80MHZ
 node_cpu = node.CPU160MHZ
 wifi_disable = 0
 wifi_ap = 1
-wifi_ap_ssid = "esp"
+wifi_ap_ssid = hostname
 wifi_sta = 0
 wifi_mode = wifi.PHYMODE_B
-wifi_stationap = 1
+wifi_stationap = 0
+wifi_sleep_type = wifi.NONE_SLEEP -- keep modem on
+-- wifi_sleep_type = wifi.LIGHT_SLEEP -- to allow the CPU to power down under some circumstances
+-- wifi_sleep_type = wifi.MODEM_SLEEP -- to power down the modem as much as possible
 
 wifi_ssid = "Sample"
 wifi_pass = "mywifi"
