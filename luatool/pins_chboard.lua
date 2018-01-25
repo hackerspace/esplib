4c0e111d01df23cde33c023cd92b3a9ed4fec9e9
Richard Marko <srk@48.io>
2018-01-25 22:03:58 +0100

    add pinout for black chinese boards with CH340G UART adapter baseboard

---
+++ luatool/pins_chboard.lua
@@ -0,0 +1,26 @@
+--
+-- 0: GPIO16 (no pwm)
+-- 1: GPIO5
+-- 2: GPIO4
+-- 3: GPIO0 (Special: Bootloader when low)
+-- 4: GPIO2 in1
+-- 5: GPIO14 (no pwm?)
+-- 6: GPIO12
+-- 7: GPIO13
+-- 8: GPIO15
+-- 9: GPIO3
+-- 10: GPIO1
+-- 11: GPIO9 (crashes)
+-- 12: GPIO10
+-- A0:
+
+
+-- For EPS12-F breakout boards with CH340G UART adapter baseboard
+
+user_sw = 2
+led_r = 8
+led_g = 6
+led_b = 7
+
+
+-- ADC - LDR (Light dependent resistor) 
